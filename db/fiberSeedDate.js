21215481937ba4bebb8401c5ae7d907abedd4b67
jotavo13 <juandotavo@gmail.com>
2023-04-15 15:31:01 -0400

    working on ejs

---
+++ db/fiberSeedDate.js
@@ -0,0 +1,7 @@
+module.exports = [
+	{ fiberID: 'scsg.1014.131242', DeviceA: 'ssdis1', TypeOfConnectorOfA: "LC", locationOfDeviceA: '1511 sumter st', TypeOfFiber: 'single mode', DeviceB: 'brrdist1', TypeOfConnectorOfB: "LC", locationOfDeviceA: 'broadriver', isItWokring: true },
+	{ fiberID: 'scsg.1014.131243', DeviceA: 'ssdis2', TypeOfConnectorOfA: "SC", locationOfDeviceA: '1511 sumter st', TypeOfFiber: 'single mode', DeviceB: 'PE2', TypeOfConnectorOfB: "LC", locationOfDeviceA: 'broadriver', isItWokring: true },
+	{ fiberID: 'scsg.1014.131244', DeviceA: 'brrdist1', TypeOfConnectorOfA: "LC", locationOfDeviceA: 'broadriver', TypeOfFiber: 'single mode', DeviceB: 'PE3', TypeOfConnectorOfB: "LC", locationOfDeviceA: '1511 sumter st', isItWokring: true },
+	{ fiberID: 'scsg.1014.131245', DeviceA: 'brrdist2', TypeOfConnectorOfA: "SC", locationOfDeviceA: 'broadriver', TypeOfFiber: 'multi mode', DeviceB: 'PE1', TypeOfConnectorOfB: "LC",locationOfDeviceA: '1511 sumter st', isItWokring: true },
+	{ fiberID: 'scsg.1014.131246', DeviceA: 'PE1', TypeOfConnectorOfA: "LC", locationOfDeviceA: '1511 sumter st', TypeOfFiber: 'multi mode', DeviceB: 'CUSTOMER_A', TypeOfConnectorOfB: "LC",locationOfDeviceA: '1511 sumter st', isItWokring: false },
+];
